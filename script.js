e6f920c140565a2e34e9a525accb4a8a6c32a813
R3ndrex <yacholohchtoli@gmail.com>
2024-09-08 17:48:19 +0300

    Add variables for displays,buttons, element of math operation

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+let operator = "";
+let firstNumber = 0;
+let secondNumber = 0;
+
+const display = document.querySelector(".display");
+const resultDisplay = document.querySelector(".display-result");
+const buttons = document.querySelectorAll(".button");
+
 const operators = {
     "+": function add(a, b) {
         return a + b;
